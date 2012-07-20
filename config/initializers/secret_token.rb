e8ec568ad9cde57aaa023cc5ce91095e8b655242
olive.naveen.santo <olive.naveen.santo@gmail.com>
2012-07-20 08:53:51 +0100

    twitter app first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitterapp::Application.config.secret_token = 'c97c8d377f6d0086a6d67498c7fedd866da981f74c4bb3ddb150173fd12a30e717af03bf3b2e1b05acb3554c4e154c1e6f30b78a552ed1b3ea9e88f6104fac92'
